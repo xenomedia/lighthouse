97ebc45b49e70ecf97047e60a53ba7ef371ecfae
Ward Peeters <ward@coding-tech.com>
2017-12-10 23:03:01 +0100

    Add audit tests

--- lighthouse-core/test/gather/gatherers/fonts-test.js
+++ lighthouse-core/test/gather/gatherers/fonts-test.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright 2016 Google Inc. All Rights Reserved.
+ * @license Copyright 2017 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
@@ -39,7 +39,7 @@ openSansFontFaces.push(Object.assign({},
   openSansFontBold,
   {
     src: [
-      'https://fonts.gstatic.com/s/opensans/v15/u-WUoqrET9fUeobQW7jkRYX0hVgzZQUfRDuZrPvH3D8.woff2',
+      'https://fonts.gstatic.com/s/opensans/v15/k3k702ZOKiLJc3WVjuplzA7aC6SjiAOpAWOKfJDfVRY.woff2',
     ],
   }
 ));
