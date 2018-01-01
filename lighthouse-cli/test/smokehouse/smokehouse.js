ee359c8470fd7a71f2c15f636e0bec1003338d88
Ward Peeters <ward@coding-tech.com>
2018-01-01 21:54:40 +0100

    Smokehouse: Convert save-assets-path to an array

--- lighthouse-cli/test/smokehouse/smokehouse.js
+++ lighthouse-cli/test/smokehouse/smokehouse.js
@@ -265,6 +265,7 @@ const cli = yargs
     'config-path': 'The path to the config JSON file',
     'expectations-path': 'The path to the expected audit results file',
   })
+  .array('save-assets-path')
   .default('config-path', DEFAULT_CONFIG_PATH)
   .default('expectations-path', DEFAULT_EXPECTATIONS_PATH)
   .argv;
@@ -276,9 +277,9 @@ const expectations = require(resolveLocalOrCwd(cli['expectations-path']));
 // reporting result.
 let passingCount = 0;
 let failingCount = 0;
-expectations.forEach(expected => {
+expectations.forEach((expected, index) => {
   console.log(`Checking '${expected.initialUrl}'...`);
-  const results = runLighthouse(expected.initialUrl, configPath);
+  const results = runLighthouse(expected.initialUrl, configPath, cli['save-assets-path'][index]);
   const collated = collateResults(results, expected);
   const counts = report(collated);
   passingCount += counts.passed;
